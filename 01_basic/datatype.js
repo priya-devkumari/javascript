69c0fea72ec2eb4fa7f442a4fe5d85ad021751b9
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-05-17 09:07:06 +0000

    container config done

---
+++ 01_basic/datatype.js
@@ -0,0 +1,14 @@
+let name="priya"
+let age=21
+let login=true
+
+// string=""
+// number=2 to power 50
+// boolean=true/false
+// null=standalone value
+// undefine=let name;
+// null
+
+// object
+console.log(typeof "priya");
+
