4f395baef66fc7fe2994e1739c887d94cf7c67c9
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-05-17 08:41:41 +0000

    container config done

---
+++ 01_basic/var.js
@@ -0,0 +1,16 @@
+const name="priya kumari"
+let email="priya@gmail.com"
+var accountpass="23456"
+accountcity="patna"
+let age;
+
+// name="pihu" not allowed
+email="kumari@gmail.com"
+ accountpass="12345"
+ accountcity="darbhanga"
+console.log(name);
+ console.table([name,email,accountpass,accountcity,age])
+
+ /*
+
+ */
