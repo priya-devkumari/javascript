b055afb2033cb372b055c30d20a47fcbc5bfd668
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-05-18 07:24:47 +0000

    datatype summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,24 @@
+//two types of datatype
+//primitive(string,boolean,number,bigint,null,undefine,symbole)
+const store=100
+const score=100.3
+const isloggIn=false
+const outside=null
+const age;
+const name ="priya"
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId)
+const bigNumber=12352663463767n
+
+
+//Non primitive(array,object,function)reference
+const fruits=[mango,orange,apple];
+let obj{
+    name:"priya",
+    age:21
+}
+let myFunction=function(){
+    console.log("hello world")
+}
+
