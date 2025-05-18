358a62b2a84b6c26e3730e300e9576e860f63da1
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-05-18 06:19:52 +0000

    container config done

---
+++ 01_basic/comparison.js
@@ -0,0 +1,14 @@
+console.log(2<1)
+console.log(3<=4)
+console.log(4>3)
+console.log(4==5)
+
+//diff data type comp
+console.log("2"==2)
+console.log(1==true)
+
+console.log(null==0)
+console.log(null>0)
+console.log(null<0)
+console.log(null<=0)
+console.log(null>=0)
