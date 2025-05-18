8f6d2746172761c69b1005cf9169bf36c1fb3148
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-05-18 05:54:53 +0000

    container config done

--- 01_basic/conversion.js
+++ 01_basic/conversion.js
@@ -1,11 +1,11 @@
 let name="priya"
 let boolean=Boolean(name)
 
- console.log(boolean);
+ //console.log(boolean);
 let islogin=33
 //let stringnumber=string(islogin);
 //console.log(islogin)
-console.log(typeof(name))
+//console.log(typeof(name))
 
 
 //"33"=>33
@@ -13,9 +13,19 @@ console.log(typeof(name))
 //true=1,false=0
 let isLoggedIn = 1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 
 
 //*********** operation***************
 
+let str1 = 2
+let str2 = 3
+let str3 = str1+str2
+console.log(str3)
+console.log(2+3)
+console.log("3"+4)
+console.log(4+"priya")
+console.log((2+3)*5%2)
+console.log("3"+4+5)
+console.log(6+5+"5")
