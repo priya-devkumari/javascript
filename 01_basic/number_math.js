797c3330e42f352ee96ad494f7da34ca04999152
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-05-20 06:05:03 +0000

    number and math

---
+++ 01_basic/number_math.js
@@ -0,0 +1,28 @@
+const score=100;
+/*console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));*/
+
+//------------math---------
+console.log(Math);
+console.log(Math.max(1,5,7,3,2));
+console.log(Math.ceil(4.5));
+console.log(Math.round(4.6));
+console.log(Math.min(3,6,1,8,6));
+
+
+console.log(Math.random());
+console.log((Math.random()*100)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
