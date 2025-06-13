dcc0dc84e65098696ccd44001d3ca4b9f948bd1d
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-06-13 03:34:40 +0000

    this

---
+++ 02_basic/this.js
@@ -0,0 +1,17 @@
+const user ={
+    username:"priya",
+    price :800,
+
+    welcomemassage : function() {
+        console.log(`${this.username}, welcome to my website`);
+       console.log(this);
+        
+        
+    }
+
+
+}
+//  user.welcomemassage()
+// user.username="pihuu";
+// user.welcomemassage()
+console.log(this);
