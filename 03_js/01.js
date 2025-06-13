01b16c7b2de8a5172993e3d053a627aee8909b04
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-06-13 04:43:03 +0000

    if statement

---
+++ 03_js/01.js
@@ -0,0 +1,25 @@
+//if
+/*const isLoggedIn = "login";
+if (isLoggedIn=="login") {
+    console.log(isLoggedIn);
+    
+}else{
+    console.log(`not loggedIn`);
+    
+}*/
+
+// const score =120;
+// if (score > 100) {
+//     const power ="fly"
+//     console.log(`user power:${power}`);
+    
+// }
+
+const balance =700;
+// if (balance > 500) console.log("test"),console.log("test2");
+
+if (balance < 500) {
+    
+}
+
+
