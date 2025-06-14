983ec09b07a7c3c7fc7ac1fe83bf20a7edc064a0
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-06-14 05:57:02 +0000

    for loop

---
+++ 03_js/loop.js
@@ -0,0 +1,16 @@
+//for loop
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element);
+    
+    
+}
+
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j <10; j++) {
+        const element = array[j];
+        
+    }
+    
+}
