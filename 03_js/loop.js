13b52155ad8e7c3adc4e384a3961e030c05a448f
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-06-17 15:14:13 +0000

    loop

--- 03_js/loop.js
+++ 03_js/loop.js
@@ -2,15 +2,37 @@
 
 for (let i = 0; i < 10; i++) {
     const element = i;
+    // console.log(element);
+    
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop: ${i}`);
+    
+    for (let j = 1; j <= 10; j++) {
+        //  console.log(`inner loop value ${j} and inner loop ${i}`);
+        // console.log(i + '*' +j + '=' + i*j);
+        
+        
+        
+    }
+    
+}
+
+
+
+let myArray =["flash","batman","superman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
     console.log(element);
     
     
 }
 
-for (let i = 0; i < 10; i++) {
-    for (let j = 0; j <10; j++) {
-        const element = array[j];
-        
-    }
+// break and continue
+
+for (let index = 0; index <= 20; index++) {
+    log
     
 }
