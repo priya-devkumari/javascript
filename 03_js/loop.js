6512d37390f6ab936ba1faa53476422cad0065f5
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-06-18 03:40:42 +0000

    for loop

--- 03_js/loop.js
+++ 03_js/loop.js
@@ -32,10 +32,22 @@ for (let index = 0; index < myArray.length; index++) {
 
 // break and continue
 
+// for (let index = 0; index <= 20; index++) {
+//     if (index==5) {
+//         console.log(`5 is the best number`);
+// break
+        
+//     }
+//     console.log(index);
+    
+    
+// }
+
+
 for (let index = 0; index <= 20; index++) {
     if (index==5) {
         console.log(`5 is the best number`);
-        
+        continue
         
     }
     console.log(index);
