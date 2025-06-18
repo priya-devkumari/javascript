d9f752d7e0d343725e061407dfebb9a18b562e48
Priya kumari <157795077+priya-devkumari@users.noreply.github.com>
2025-06-18 03:59:29 +0000

    while and dowhile

---
+++ 03_js/while.js
@@ -0,0 +1,21 @@
+let score =0
+while (score <= 20) {
+    //console.log(score);
+    score=score+2
+}
+
+//array
+let myArray =["flash","batman","superman"]
+let arr =0
+while (arr < myArray.length) {
+    //console.log(myArray[arr]);
+    arr = arr+1
+    
+}
+//do while
+let marks =10
+do {
+    console.log(`marks is ${marks}`);
+    marks=marks+10
+} while (marks <= 100);
+
